5512113918ec93bdbb3e079e9ebfc40ea0b91b67
Sajol Das <sajoldas421@example.com>
2023-09-25 05:32:34 +0600

    cart

---
+++ frontend/src/constants.js
@@ -0,0 +1,11 @@
+//export const BASE_URL = process.env.NODE_ENV === 'developments' ?
+//'http://localhost:5000' : '';
+
+export const BASE_URL = '';
+export const PRODUCTS_URL = '/api/products';
+
+export const USERS_URL = '/api/users';
+
+export const ORDERS_URL = '/api/orders';
+
+export const BKASH_URL = '/api/config/bkash';
